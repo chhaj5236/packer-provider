bedb2e7934a502430ddea0dbaad6d14020e64faf
bozhi.ch <bozhi.ch@alibaba-inc.com>
2018-11-28 23:10:40 +0800

    WIP: support snapshot names

--- ecs/image_config.go
+++ ecs/image_config.go
@@ -27,12 +27,14 @@ type AlicloudDiskDevices struct {
 
 type AlicloudImageConfig struct {
 	AlicloudImageName                 string            `mapstructure:"image_name"`
+	AlicloudImageSnapshotNames                 []string            `mapstructure:"image_snapshot_names"`
 	AlicloudImageVersion              string            `mapstructure:"image_version"`
 	AlicloudImageDescription          string            `mapstructure:"image_description"`
 	AlicloudImageShareAccounts        []string          `mapstructure:"image_share_account"`
 	AlicloudImageUNShareAccounts      []string          `mapstructure:"image_unshare_account"`
 	AlicloudImageDestinationRegions   []string          `mapstructure:"image_copy_regions"`
 	AlicloudImageDestinationNames     []string          `mapstructure:"image_copy_names"`
+	AlicloudImageDestinationSnapshotNames     map[string][]string         `mapstructure:"image_copy_snapshot_names"`
 	AlicloudImageForceDelete          bool              `mapstructure:"image_force_delete"`
 	AlicloudImageForceDeleteSnapshots bool              `mapstructure:"image_force_delete_snapshots"`
 	AlicloudImageForceDeleteInstances bool              `mapstructure:"image_force_delete_instances"`
@@ -46,15 +48,34 @@ func (c *AlicloudImageConfig) Prepare(ctx *interpolate.Context) []error {
 	var errs []error
 	if c.AlicloudImageName == "" {
 		errs = append(errs, fmt.Errorf("image_name must be specified"))
-	} else if len(c.AlicloudImageName) < 2 || len(c.AlicloudImageName) > 128 {
-		errs = append(errs, fmt.Errorf("image_name must less than 128 letters and more than 1 letters"))
-	} else if strings.HasPrefix(c.AlicloudImageName, "http://") ||
-		strings.HasPrefix(c.AlicloudImageName, "https://") {
-		errs = append(errs, fmt.Errorf("image_name can't start with 'http://' or 'https://'"))
+	} else {
+		if partErrs := validateImageName(c.AlicloudImageName, "image_name"); partErrs != nil {
+			errs = append(errs, partErrs...)
+		}
 	}
-	reg := regexp.MustCompile("\\s+")
-	if reg.FindString(c.AlicloudImageName) != "" {
-		errs = append(errs, fmt.Errorf("image_name can't include spaces"))
+
+	if len(c.AlicloudImageDestinationNames) > 0 {
+		for index, destName := range c.AlicloudImageDestinationNames {
+			if destName == "" {
+				continue
+			}
+
+			if partErrs := validateImageName(destName, fmt.Sprintf("image_copy_names[%d]", index)); partErrs != nil {
+				errs = append(errs, partErrs...)
+			}
+		}
+	}
+
+	if len(c.AlicloudImageSnapshotNames) > 0 {
+		for index, snapshotName := range c.AlicloudImageSnapshotNames {
+			if snapshotName == "" {
+				continue
+			}
+
+			if partErrs := validateSnapshotName(snapshotName, fmt.Sprintf("image_snapshot_names[%d]", index)); partErrs != nil {
+				errs = append(errs, partErrs...)
+			}
+		}
 	}
 
 	if len(c.AlicloudImageDestinationRegions) > 0 {
@@ -72,8 +93,8 @@ func (c *AlicloudImageConfig) Prepare(ctx *interpolate.Context) []error {
 
 			if !c.AlicloudImageSkipRegionValidation {
 				// Verify the region is real
-				if valid := validateRegion(region); valid != nil {
-					errs = append(errs, fmt.Errorf("Unknown region: %s", region))
+				if err := validateRegion(region); err != nil {
+					errs = append(errs, err)
 					continue
 				}
 			}
@@ -82,6 +103,65 @@ func (c *AlicloudImageConfig) Prepare(ctx *interpolate.Context) []error {
 		}
 
 		c.AlicloudImageDestinationRegions = regions
+	}
+
+	if len(c.AlicloudImageDestinationSnapshotNames) > 0 {
+		for region, snapshotNames := range c.AlicloudImageDestinationSnapshotNames {
+			if !c.AlicloudImageSkipRegionValidation {
+				if err := validateRegion(region); err != nil {
+					errs = append(errs, err)
+				}
+			}
+
+			for index, snapshotName := range snapshotNames {
+				if snapshotName == "" {
+					continue
+				}
+
+				if partErrs := validateSnapshotName(snapshotName, fmt.Sprintf("image_snapshot_names[%d]", index)); partErrs != nil {
+					errs = append(errs, partErrs...)
+				}
+			}
+		}
+	}
+
+	if len(errs) > 0 {
+		return errs
+	}
+
+	return nil
+}
+
+func validateImageName(name string, option string) []error {
+	var errs []error
+
+	if len(name) < 2 || len(name) > 128 {
+		errs = append(errs, fmt.Errorf("%s must less than 128 letters and more than 1 letters", option))
+	} else if strings.HasPrefix(name, "http://") || strings.HasPrefix(name, "https://") {
+		errs = append(errs, fmt.Errorf("%s can't start with 'http://' or 'https://'", option))
+	}
+
+	reg := regexp.MustCompile("\\s+")
+	if reg.FindString(name) != "" {
+		errs = append(errs, fmt.Errorf("%s can't include spaces", name))
+	}
+
+	if len(errs) > 0 {
+		return errs
+	}
+
+	return nil
+}
+
+func validateSnapshotName(name string, option string) []error{
+	errs := validateImageName(name, option)
+
+	if strings.HasPrefix(name, "auto"){
+		if errs == nil {
+			errs = []error{}
+		}
+
+		errs = append(errs, fmt.Errorf("%s can't start with 'auto'", option))
 	}
 
 	if len(errs) > 0 {
